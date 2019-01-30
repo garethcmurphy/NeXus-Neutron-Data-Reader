ed24a8ea53bda138368eeea41f1caac81c00612e
Gareth Murphy <garethcmurphy@gmail.com>
2019-01-30 14:20:15 +0100

    add formatting

--- x.ts
+++ x.ts
@@ -1,22 +1,22 @@
-var hdf5 = require('hdf5').hdf5;
-var h5lt = require('hdf5').h5lt;
+var hdf5 = require("hdf5").hdf5;
+var h5lt = require("hdf5").h5lt;
 
- 
-var Access = require('hdf5/lib/globals').Access;
+var Access = require("hdf5/lib/globals").Access;
 
 class GetH5Info {
-	filename:string;
-constructor(){
-	this.filename="V20_ESSIntegration_2018-12-10_1009.nxs";
+  filename: string;
+  constructor() {
+    this.filename = "V20_ESSIntegration_2018-12-10_1009.nxs";
+  }
+  getInfo() {
+    var file = new hdf5.File(this.filename, Access.ACC_RDONLY);
+    var group = file.openGroup("/entry/ESS_users/");
+    var members = group.getMemberNames();
+    console.log(members);
+    var array = h5lt.readDataset(group.id, "name");
+    console.log(array);
+  }
 }
-	getInfo (){
-var file = new hdf5.File(this.filename, Access.ACC_RDONLY);
-var group = file.openGroup('/entry/ESS_users/');
-var array=h5lt.readDataset(group.id, 'name');
- console.log(array);
- }
-}
-
 
 const h5 = new GetH5Info();
 h5.getInfo();
