116d4d688ee9c355caf0ddb7c591b2d19ca9d6ec
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-01 15:31:34 +0100

    ad tag

--- x.py
+++ x.py
@@ -11,6 +11,45 @@ class GetH5Info:
     filename = "v20.h5"
     metadata = {}
 
+    sourceFolderArray = {
+        "0001": "v20/2018_01_24",
+        "0002": "v20/2018_12_13/v20-2018-12-14T11:22:26+0100",
+        "0003": "v20/2018_12_13/v20-2018-12-14T11:23:38+0100",
+        "0004": "v20/2018_12_13/v20-2018-12-14T15:12:53+0100",
+        "0005": "v20/2018_12_13/v20-2018-12-14T16:12:01+0100",
+        "0006": "v20/2018_12_13/v20-2018-12-14T16:12:26+0100",
+        "0007": "v20/2018_12_13/v20-2018-12-15T16:56:22+0100",
+        "0008": "v20/2018_12_13/v20-2018-12-15T18:10:22+0100",
+        "0009": "v20/2018_12_13/v20-2018-12-17T07:13:37+0100",
+        "0010": "v20/2018_12_13/v20-2018-12-17T19:31:02+0100",
+        "0011": "v20/2018_12_13/v20-2018-12-18T09:18:46+0100",
+        "0012": "v20/2018_12_13/v20-2018-12-18T10:42:33+0100",
+        "0013": "v20/2018_12_13/v20-2018-12-18T11:01:28+0100",
+        "0014": "v20/2018_12_13/v20-2018-12-19T08:17:32+0100",
+        "0015": "v20/2018_12_13/v20-2018-12-19T08:56:00+0100",
+        "0016": "v20/2018_12_13/V20_ESSIntegration_20181210",
+        "0017": "v20/2018_12_13/V20_ESSIntegration_2018-12-10_1009",
+        "0018": "v20/2018_12_13/V20_ESSIntegration_2018-12-10_1805",
+        "0019": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_0915",
+        "0020": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_0943",
+        "0021": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_0951",
+        "0022": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_0952",
+        "0023": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_1018",
+        "0024": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_1713",
+        "0025": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_1743",
+        "0026": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_1847",
+        "0027": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_1914",
+        "0028": "v20/2018_12_13/V20_ESSIntegration_2018-12-11_1923",
+        "0029": "v20/2018_12_13/V20_ESSIntegration_2018-12-12_1209",
+        "0030": "v20/2018_12_13/V20_ESSIntegration_2018-12-13_0942",
+        "0031": "v20/2018_12_13/V20_ESSIntegration_2018-12-13_1028",
+        "0032": "v20/2018_12_13/V20_ESSIntegration_2018-12-13T15:53:48",
+        "0033": "v20/2018_12_13/V20_ESSIntegration_2018-12-13T16:20:00",
+        "0034": "v20/2018_12_13/V20_ESSIntegration_2018-12-14T09:25:00"
+    }
+
+    inv_map = {v: k for k, v in sourceFolderArray.items()}
+
     def __init__(self):
         self.filename = "v20.h5"
 
@@ -35,7 +74,11 @@ class GetH5Info:
         self.nexusInfo["source_name"] = source_name
         f.close()
         # print(self.nexusInfo)
-        self.metadata[filename] = self.nexusInfo
+        tag = filename.replace("/users/detector/experiments/", "")
+        sep = "/"
+        tag2 = sep.join(tag.split(sep)[:-1])
+        tag3 = self.inv_map[tag2]
+        self.metadata[tag3] = self.nexusInfo
 
     def get_names(self, my_list, f, tag):
         if tag in f:
@@ -62,7 +105,7 @@ class GetH5Info:
         return dset[...]
 
     def loop(self):
-        hostname=socket.gethostname()
+        hostname = socket.gethostname()
         filenames = [
             "/users/detector/experiments/v20/2018_12_13/v20-2018-12-18T09:18:46+0100/v20-2018-12-18T09:18:46+0100.nxs",
             "/users/detector/experiments/v20/2018_12_13/v20-2018-12-14T16:12:01+0100/v20-2018-12-14T16:12:01+0100.nxs",
@@ -98,8 +141,9 @@ class GetH5Info:
             "/users/detector/experiments/v20/2018_12_13/V20_ESSIntegration_2018-12-10_1805/V20_ESSIntegration_2018-12-10_1805.nxs",
             "/users/detector/experiments/v20/2018_12_13/v20-2018-12-14T16:12:26+0100/v20-2018-12-14T16:12:26+0100.nxs",
         ]
-        if hostname== 'CI0020036':
-            filenames = ["v20.h5"]
+        if hostname == 'CI0020036':
+            filenames = [
+                "/Users/garethmurphy/Downloads/data/V20_ESSIntegration_2018-12-10_1009.nxs"]
         for filename in filenames:
             self.get_h5_info(filename)
 
@@ -111,6 +155,6 @@ if __name__ == "__main__":
     print(json.dumps(h5.metadata, indent=2, sort_keys=True))
 
     filename = "metadata.json"
-        # Writing JSON data
+    # Writing JSON data
     with open(filename, 'w') as f:
         json.dump(h5.metadata, f)
